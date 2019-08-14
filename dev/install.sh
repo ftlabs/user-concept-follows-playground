bb9a9b3b0e1413206e050df0150234f6eb87b71a
Jamie Brown <jamiebrown201@hotmail.com>
2019-08-14 11:34:30 +0100

    add installation script

---
+++ dev/install.sh
@@ -0,0 +1,18 @@
+HOOK_NAMES="applypatch-msg pre-applypatch post-applypatch pre-commit prepare-commit-msg commit-msg post-commit pre-rebase post-checkout post-merge pre-receive update post-receive post-update pre-auto-gc"
+GIT_HOOK_DIR=../.git/hooks
+LOCAL_HOOK_DIR=./git-hooks
+LNS_RELATIVE_PATH=../../dev/git-hooks
+
+echo "Install project git hooks"
+
+for hook in $HOOK_NAMES; do
+    if [ -f $LOCAL_HOOK_DIR/$hook ]; then
+      echo "> Hook $hook"
+      if [ ! -h $GIT_HOOK_DIR/$hook -a -x $GIT_HOOK_DIR/$hook ]; then
+          echo " > Old git hook $hook disabled"
+          mv $GIT_HOOK_DIR/$hook $GIT_HOOK_DIR/$hook.local
+      fi
+      echo " > Enable project git hook"
+      ln -s -f $LNS_RELATIVE_PATH/$hook $GIT_HOOK_DIR/$hook
+    fi
+done
